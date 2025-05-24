4caf998c3aced33d817635507c957306965d663c
Zenith <2985395460@qq.com>
2025-05-24 14:13:49 +0800

    系统页面待开发中...

--- src/pages/HomeLayout/SystemManage/index.js
+++ src/pages/HomeLayout/SystemManage/index.js
@@ -3,7 +3,7 @@ import MySwiper from "@/components/Swiper/swiper";
 const SystemManage = () => {
   return (
     <div>
-      这是系统管理页面
+      此页面正在开发中
       <a href="http://flyfish-demo.cloudwise.com/www/web/screen/index.html?viewTag=954405807350353920" target="blank">点击去数据大屏</a>
       <a href="https://vue.mtruning.club/#/chart/preview/id_35noc1pc3gi000" target="blank">点击去数据大屏</a>
       <hr></hr>
